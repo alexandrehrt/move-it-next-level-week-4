a338e38a5ea44552e1612d8d68532772aab38dde
Alexandre <alexandrehrtm@protonmail.com>
2021-02-24 19:42:55 -0300

    context

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,5 +1,12 @@
 import { ReactNode, useState, createContext } from "react";
 
+import challenges from '../../challenges.json';
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
 
 interface ChallengesProviderProps {
   children: ReactNode;
@@ -9,8 +16,11 @@ interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  experienceToNextLevel: number;
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
@@ -19,13 +29,25 @@ export function ChallengesProvider({ children }:ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    console.log('new challenge');
+    // Quando o cronômetro zerar, irá selecionar um novo desafio ao usuário
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
+  };
+
+  function resetChallenge() {
+    // Função chamada quando o usuário falhar o
+    setActiveChallenge(null);
   }
 
   return (
@@ -34,9 +56,13 @@ export function ChallengesProvider({ children }:ChallengesProviderProps) {
         level, 
         currentExperience, 
         challengesCompleted,
+        activeChallenge,
+        experienceToNextLevel,
         levelUp,
-        startNewChallenge}}
-        >
+        startNewChallenge,
+        resetChallenge
+      }}
+    >
       { children }
     </ChallengesContext.Provider>
   );
