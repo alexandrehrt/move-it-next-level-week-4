786c9b708853f3db82277c595906aa8ffa376945
Alexandre <alexandrehrtm@protonmail.com>
2021-02-24 18:03:12 -0300

    layout finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 
+import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar'
@@ -21,6 +22,10 @@ export default function Home() {
           <Profile />
           <CompletedChallenges />
           <Countdown />
+        </div>
+
+        <div>
+          <ChallengeBox />
         </div>
       </section>
   </div>
